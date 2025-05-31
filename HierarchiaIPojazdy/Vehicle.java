96c06cffd4d3688d75ce68c3ebbfdd136da01914
alexisss16 <s34802@pjwstk.edu.pl>
2025-05-31 21:54:01 +0200

    Add files via upload

---
+++ HierarchiaIPojazdy/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    public void start() {
+        System.out.println("Pojazd uruchomiony.");
+    }
+
+    public void stop() {
+        System.out.println("Pojazd zatrzymany.");
+    }
+}
